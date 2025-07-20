e34bc97a512992a87ffb9e3e1045e3a4d89f12b3
luismede <luishenrique23hh23@gmail.com>
2025-07-20 00:09:48 -0300

    feat(service): Adicionar UsuarioService com método para inserir usuário e validações necessárias

---
+++ src/main/java/io/github/nutria/nutria/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package io.github.nutria.nutria.service;
+
+import io.github.nutria.nutria.model.dao.UsuarioDAO;
+import io.github.nutria.nutria.model.entity.Usuario;
+
+public class UsuarioService {
+
+    private final UsuarioDAO dao = new UsuarioDAO();
+
+    public String insertUser(Usuario usuario) {
+        // Inicializando os atributos do usuário
+        String nome_completo = usuario.getNomeCompleto();
+        String endereco_email = usuario.getEnderecoEmail();
+        String senha = usuario.getSenha();
+        String telefone = usuario.getTelefone();
+        String empresa = usuario.getEmpresa();
+        String foto = usuario.getFoto();
+
+        // Validando se os campos obrigatórios estão preenchidos
+        if (nome_completo == null || endereco_email == null || senha == null || nome_completo.isBlank() ||
+                endereco_email.isBlank() || senha.isBlank()) {
+            return "Nome Completo, Endereço de e-mail e Senha são campos obrigatórios.";
+        }
+
+        // Validando se o e-mail é válido
+        if (dao.emailUsed(endereco_email)) {
+            return "E-mail " + endereco_email + " já cadastrado no sistema.";
+        }
+
+        // Inserindo o usuário no banco de dados
+        dao.insertUser(usuario);
+        return null;
+    }
+}
