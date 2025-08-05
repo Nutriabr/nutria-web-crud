76930e89cc4c2810bc0753922e968a419851a90f
luismede <luishenrique23hh23@gmail.com>
2025-08-05 08:32:28 -0300

    feat: criar interface IUsuarioDAO com os métodos iniciais de insert.

---
+++ src/main/java/io/github/nutria/nutria/model/dao/IUsuarioDAO.java
@@ -0,0 +1,8 @@
+package io.github.nutria.nutria.model.dao;
+
+import io.github.nutria.nutria.model.entity.Usuario;
+
+public interface IUsuarioDAO {
+    boolean emailUsed(String email);
+    void insertUser(Usuario usuario);
+}
