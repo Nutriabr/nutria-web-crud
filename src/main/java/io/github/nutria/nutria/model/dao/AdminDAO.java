5021c463345530b21c215a0a3c0be2dbe8f9c764
luismede <luishenrique23hh23@gmail.com>
2025-08-14 13:33:33 -0300

    feat(AdminDAO): Implementar insertAdmin, método para adicionar admin

--- src/main/java/io/github/nutria/nutria/model/dao/AdminDAO.java
+++ src/main/java/io/github/nutria/nutria/model/dao/AdminDAO.java
@@ -1,4 +1,18 @@
 package io.github.nutria.nutria.model.dao;
 
-public class AdminDAO {
+import io.github.nutria.nutria.model.entity.Admin;
+import io.github.nutria.nutria.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class AdminDAO implements IAdminDAO {
+    ConnectionFactory factory = new ConnectionFactory();
+    public void insertAdmin(Admin admin) {
+        try (Connection connect = factory.connect()) {
+            String sql = "INSERT INTO admin(nome, ";
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
