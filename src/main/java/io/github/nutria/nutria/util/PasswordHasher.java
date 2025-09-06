5c1dbc6c61265ab5d73121679db0651f3d8acd7d
luismede <luishenrique23hh23@gmail.com>
2025-09-05 21:50:14 -0300

    refactor(PasswordHasher, PasswordVerifier): adicionar documentação aos métodos de hash e verificação de senha

--- src/main/java/io/github/nutria/nutria/util/PasswordHasher.java
+++ src/main/java/io/github/nutria/nutria/util/PasswordHasher.java
@@ -3,10 +3,14 @@ package io.github.nutria.nutria.util;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHasher {
+
     private final static int workload = 12;
 
+    /** Método estático responsável por gerar o hash da semanha, usando um workload 12
+     * @return String - Retorna a senha com o hash armazenado
+     * @param plaintextPassword - Recebe a senha da requisição que será feita o hash
+     * */
     public static String hashPassword(String plaintextPassword) {
         return BCrypt.hashpw(plaintextPassword, BCrypt.gensalt(workload));
     }
-
 }
--- src/main/java/io/github/nutria/nutria/util/PasswordVerifier.java
+++ src/main/java/io/github/nutria/nutria/util/PasswordVerifier.java
@@ -3,6 +3,13 @@ package io.github.nutria.nutria.util;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordVerifier {
+
+    /** Método estático responsável por verificar se a senha original condiz
+     * com o hash correto.
+     * @return boolean - Retorna true caso a senha seja igual o hash armazenado
+     * @param plaintextPassword - Recebe a senha da requisição que será feita o hash
+     * @param storedHash - Recebe o hash armazenado
+     * */
     public static boolean checkPassword(String plaintextPassword, String storedHash) {
         return BCrypt.checkpw(plaintextPassword, storedHash);
     }
