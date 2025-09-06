46f48cc2f26195f43d469208ed6c7842d30d4712
luismede <luishenrique23hh23@gmail.com>
2025-09-05 21:52:36 -0300

    refactor(AdminDAO): renomear pacote e métodos, implementar interface GenericDAO e adicionar métodos de CRUD

---
+++ src/main/java/io/github/nutria/nutria/dao/AdminDAO.java
@@ -0,0 +1,30 @@
+package io.github.nutria.nutria.dao;
+
+import io.github.nutria.nutria.model.Admin;
+import io.github.nutria.nutria.util.ConnectionFactory;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class AdminDAO implements GenericDAO<Admin, Long>, AutoCloseable {
+
+    @Override
+    public boolean insert(Admin entity) {
+        return false;
+    }
+
+    @Override
+    public List<Admin> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public boolean deleteById(Long aLong) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
