c9f5217f95e4b4f43e528191d3b4c71521474678
luismede <luishenrique23hh23@gmail.com>
2025-09-05 21:46:33 -0300

    refactor(UsuarioService): Refatorar método insert e atualizar deleteUserById

--- src/main/java/io/github/nutria/nutria/service/UsuarioService.java
+++ src/main/java/io/github/nutria/nutria/service/UsuarioService.java
@@ -1,56 +1,65 @@
 package io.github.nutria.nutria.service;
 
-import io.github.nutria.nutria.model.dao.UsuarioDAO;
-import io.github.nutria.nutria.model.entity.Usuario;
+import io.github.nutria.nutria.dao.UsuarioDAO;
+import io.github.nutria.nutria.model.Usuario;
 
 import java.util.List;
 
-public class UsuarioService {
 
+/** Classe service para métodos reutilizáveis que contém a lógica de negócio de validações, isolando-a do DAO e a
+ * Controller
+ * @see UsuarioDAO
+ * @author Luis Henrique
+ * @version 1.1
+ * */
+public class UsuarioService {
+    // 1. Instanciar o objeto dao do usuario
     private final UsuarioDAO dao = new UsuarioDAO();
 
-    public String save(Usuario usuario) {
-        // Inicializando os atributos do usuário
+    /**
+     * @param usuario Recebe o objeto usuário da Controller para validação dos campos
+     * @return string Retorna uma mensagem de sucesso ou falha para a Controller
+     */
+    public String insert(Usuario usuario) {
+        // 1. Inicializando os atributos do usuário
         String nome = usuario.getNome();
         String endereco_email = usuario.getEmail();
         String senha = usuario.getSenha();
         String telefone = usuario.getTelefone();
-        String empresa = usuario.getEmpresa();
-        String foto = usuario.getFoto();
 
-        // Validando se os campos obrigatórios estão preenchidos
+        // 1. Validando se os campos obrigatórios estão preenchidos e retornando mensagem de erro
         if (nome == null || endereco_email == null || senha == null || nome.isBlank() ||
                 endereco_email.isBlank() || senha.isBlank()) {
             return "Nome Completo, Endereço de e-mail e Senha são campos obrigatórios.";
         }
 
-        // Validando se a senha está correta
+        // 2. Método para validar se a senha tem mais de 8 caracteres e retorna mensagem de erro
         if (senha.length() <= 8) {
             return "A senha deve conter quantidade de caracteres maior ou igual a 8";
         }
 
-        // Validando se o telefone possui 11 caracteres
-        // TODO: Corrigir a quantidade de caracteres no banco
-        if (telefone.length() != 11) {
-            return "A quantidade de caracteres do Telefone deve conter 11 caracteres";
-        }
-
-        // Validando se o e-mail é válido
+        /*
+        * 3. Método para retornar se o email já está cadastrado no Banco de Dados e retorna mensagem de erro
+        */
         if (dao.findByEmailUsed(endereco_email)) {
             return "E-mail " + endereco_email + " já cadastrado no sistema.";
         }
 
-        // Inserindo o usuário no banco de dados
-        dao.save(usuario);
-        return null;
+        // 4. Inserindo o usuário no banco de dados e retornando mensagem de sucesso ou falha
+        if (dao.insert(usuario)) {
+            return "Usuário cadastrado no banco de dados.";
+        }
+        return "Não foi possível cadastrar o usuário no banco de dados";
 
     }
 
+    // 2. Método para listar todos os usuários
     public List<Usuario> findAll() {
         return dao.findAll();
     }
 
-    public boolean deleteUserById(int id) {
-        return dao.deleteUserById(id) == 1;
+    // 3. Método para deletar usuário por ID e retornar boolean com sucesso ou falha
+    public boolean deleteUserById(long id) {
+        return dao.deleteById(id);
     }
 }
