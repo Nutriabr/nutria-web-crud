787d30e2a32d6fc93c21c05b6869f18885c9f696
luismede <luishenrique23hh23@gmail.com>
2025-09-05 21:48:34 -0300

    refactor(ConnectionFactory): substituir dotenv por System.getenv e melhorar documentação

--- src/main/java/io/github/nutria/nutria/util/ConnectionFactory.java
+++ src/main/java/io/github/nutria/nutria/util/ConnectionFactory.java
@@ -1,42 +1,37 @@
 package io.github.nutria.nutria.util;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/** Classe responsável por estabelecer e fechar a conexão com o banco de dados
+ * @author luismedeiros-ieg
+ * @version 1.1
+ * */
 public class ConnectionFactory {
-    // Carregando as variáveis de ambiente do arquivo .env
-    static Dotenv dotenv = Dotenv.configure().load();
 
-    // Método para estabelecer a conexão com o banco de dados
+    /** Método para estabelecer conexão com o banco de dados com base no Driver e informações de
+     * Host do banco
+     * @author luismedeiros-ieg
+     * @return conn - Retorna a conexão do DriverManager
+     * */
     public static Connection connect() {
         Connection con = null;
         try {
+
+            // Utilizando as variáveis de ambiente configuradas no .env com as informações sensíveis do banco de dados
             final String DRIVER = "org.postgresql.Driver";
-            final String URL = dotenv.get("DB_URL");
-            final String USER = dotenv.get("DB_USER");
-            final String PASSWORD = dotenv.get("DB_PASSWORD");
+            final String URL = System.getenv("DB_URL");
+            final String USER = System.getenv("DB_USER");
+            final String PASSWORD = System.getenv("DB_PASSWORD");
 
             Class.forName(DRIVER);
+
+            // Tentativa de estabelecer conexão com a URL do banco de dados fornecida.
             con = DriverManager.getConnection(URL, USER, PASSWORD);
-            // Estabelecendo conexão com o banco
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             return con;
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    // Método para testar a conexão com o banco de dados
-    private void testConnection() {
-        try (Connection con = connect();) {
-
-            System.out.println(con);
-
-            // Fechando a conexão com o banco de dados
-//            con.close();
-        } catch (Exception e) {
-            throw new RuntimeException("Ocorreu um erro na conexão com o Banco de dados: " + e.getMessage());
         }
     }
 }
