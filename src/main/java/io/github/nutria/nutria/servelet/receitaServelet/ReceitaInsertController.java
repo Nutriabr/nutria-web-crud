3018a274989831dd2ed88db202599d5023875773
marianamarrao <marimarrao0115@gmail.com>
2025-10-03 08:32:53 -0300

    feat(ReceitaInsertController): Adicionar classe Controller de Receita para inserir os dados

---
+++ src/main/java/io/github/nutria/nutria/servelet/receitaServelet/ReceitaInsertController.java
@@ -0,0 +1,63 @@
+package io.github.nutria.nutria.servelet.receitaServelet;
+
+import io.github.nutria.nutria.dao.ReceitaDAO;
+import io.github.nutria.nutria.model.Receita;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "receitaController", urlPatterns = {"/receitas", "/receitas/inserir"})
+public class ReceitaInsertController extends HttpServlet {
+    // 1. Instância do DAO para acessar o banco de dados
+    private final ReceitaDAO dao = new ReceitaDAO();
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        // 2. Recuperar o caminho da URL da requisição
+        String caminhoUrl = request.getServletPath();
+
+        // 3. Declaração das variáveis
+        String nome;
+        String porcao;
+        String idProdutoStr;
+        long idProduto;
+        boolean error;
+        Receita receita;
+
+        if(caminhoUrl.equals("/receitas/inserir")){
+            // 4. Requisitar os parâmetros enviados
+            nome = request.getParameter("nome");
+            porcao = request.getParameter("porcao");
+            idProdutoStr = request.getParameter("idProduto");
+            idProduto = 0;
+            if (idProdutoStr != null && !idProdutoStr.isBlank()) {
+                idProduto = Long.parseLong(idProdutoStr);
+            }
+
+            // 5. Instanciação do objeto Receita com os dados obtidos
+            receita = new Receita(nome,porcao,idProduto);
+
+            // 6. Validação dos campos obrigatórios
+            if(porcao == null ||  porcao.isBlank() || idProduto <=0 ){
+                response.setStatus(400);
+                response.getWriter().write("Os campos de porção e id do produto são obrigatórios.");
+                return;
+            }
+
+            // 7. Chamada do método insert
+            error = dao.insert(receita);
+
+            // 8. Se tiver não tiver erro, retornar 201 (Created)
+            if(error){
+                response.setStatus(201);
+            }
+
+            // 9. Se tiver erro, retornar 400 (Bad Request)
+            else {
+                response.setStatus(400);
+            }
+
+        }
+    }
+}
