34ed346d832e2bf5b88e09d991b0e1069a3f6e64
luismede <luishenrique23hh23@gmail.com>
2025-10-04 18:54:08 -0300

    feat(AdminDAO): Implementar interface IAdminDAO e métodos adicionais para gerenciamento de administradores

--- src/main/java/io/github/nutria/nutria/dao/AdminDAO.java
+++ src/main/java/io/github/nutria/nutria/dao/AdminDAO.java
@@ -1,18 +1,16 @@
 package io.github.nutria.nutria.dao;
 
 import io.github.nutria.nutria.dao.interfaces.GenericDAO;
+import io.github.nutria.nutria.dao.interfaces.IAdminDAO;
 import io.github.nutria.nutria.model.Admin;
 import io.github.nutria.nutria.model.Usuario;
 import io.github.nutria.nutria.util.ConnectionFactory;
 import io.github.nutria.nutria.util.PasswordHasher;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
-public class AdminDAO {
+public class AdminDAO implements GenericDAO<Admin, Long>, IAdminDAO {
     public boolean findByEmailUsed(String email) {
         String sql = "SELECT COUNT(*) FROM admin WHERE email = ?";
 
@@ -26,10 +24,9 @@ public class AdminDAO {
             connect = ConnectionFactory.connect();
             ps = connect.prepareStatement(sql);
             ps.setString(1, email);
-            // 2. Executar a consulta
+
             rs = ps.executeQuery();
 
-            // 3. Verificar se há linhas encontradas
             if (rs.next()) {
                 /* 4. Se o resultado da função COUNT for maior que 0, significa que
                  *  o e-mail já está cadastrado
@@ -37,28 +34,19 @@ public class AdminDAO {
                 result = rs.getInt(1) > 0;
             }
         } catch (SQLException e) {
-            // 5. Tratar exceções de SQL
             e.printStackTrace();
             return false;
         } finally {
             try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-
-                if (connect != null) {
-                    ConnectionFactory.disconnect(connect);
-                }
+                if (ps != null) ps.close();
+                if (rs != null) rs.close();
+                if (connect != null) ConnectionFactory.disconnect(connect);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
         return result;
     }
-
 
     public Admin findByEmail(String email) {
         String sql = "SELECT * FROM admin WHERE email = ?";
@@ -129,6 +117,50 @@ public class AdminDAO {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public List<Admin> findAll(int page) {
+        return List.of();
+    }
+
+    @Override
+    public boolean deleteById(Long aLong) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int countAll() {
+        int totalAdmins = 0;
+
+        String sql = "SELECT COUNT(*) FROM admin";
+
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection connect = null;
+
+        try {
+            connect = ConnectionFactory.connect();
+
+            stmt = connect.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            if (rs.next()) {
+                totalAdmins = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connect != null) ConnectionFactory.disconnect(connect);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return totalAdmins;
     }
 
 }
