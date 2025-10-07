b9a3a19dc54847455c9cc6f56661c31ca48c794b
GiovannaVeloso <giovannmveloso09@gmail.com>
2025-10-07 10:47:17 -0300

    fix(TabelaNutricional): Remover importação do pacote service

--- src/main/java/io/github/nutria/nutria/servlet/tabelaNutricionalServlet/TabelaNutricionalServlet.java
+++ src/main/java/io/github/nutria/nutria/servlet/tabelaNutricionalServlet/TabelaNutricionalServlet.java
@@ -1,6 +1,5 @@
 package io.github.nutria.nutria.servlet.tabelaNutricionalServlet;
 
-import io.github.nutria.nutria.service.TabelaNutricionalService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,19 +12,11 @@ import java.io.IOException;
 public class TabelaNutricionalServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TabelaNutricionalService service = new TabelaNutricionalService();
-        String path = req.getServletPath();
-        if (path.equals("/tabela_nutricional/inserir")) {
-            service.save(req);
-        }
+
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String path = req.getServletPath();
-        TabelaNutricionalService service = new TabelaNutricionalService();
-        if (path.equals("/tabela_nutricional")) {
-            resp.getWriter().write(service.findAll().toString());
-        }
+
     }
 }
