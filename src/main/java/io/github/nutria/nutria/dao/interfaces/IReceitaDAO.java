5cacb6a6848819c9d39fbd95df680ba39a71fc46
giiovannasantoss <gihalmeida127@gmail.com>
2025-10-10 15:25:10 -0300

    docs(interfaces): (wip) padronizar Javadoc existentes nas interfaces

--- src/main/java/io/github/nutria/nutria/dao/interfaces/IReceitaDAO.java
+++ src/main/java/io/github/nutria/nutria/dao/interfaces/IReceitaDAO.java
@@ -1,20 +1,26 @@
 package io.github.nutria.nutria.dao.interfaces;
 
+import io.github.nutria.nutria.exceptions.DataAccessException;
 import io.github.nutria.nutria.model.Receita;
 
 import java.util.List;
 
 /**
- * Interface para operações de CRUD na entidade Receita.
- * @author Mariana Marrão
- * @version 1.1
+ * Interface para operações específicas de CRUD na entidade {@link Receita}.
+ *
  * @see Receita
+ * @author Mariana Marrão
+ * @version 1.0
  */
 public interface IReceitaDAO {
+
     /**
-     * Método para achar receitas pela porção
-     * @param porcao Recebe como o parametro a porção que será usado na clausura where na query
-     * @return List<Receita> Retorna a lista de todas as receitas achadas no banco com determinada porção
+     * Busca registros de {@link Receita} com a porção informada.
+     *
+     * @param porcao a porção que será utilizada na busca.
+     * @return uma lista de objetos {@link Receita} correspondentes à porção informada.
+     * @throws DataAccessException se ocorrer algum erro ao acessar o banco de dados.
+     * @author Mariana Marrão
      */
     List<Receita> findByPorcao(String porcao);
 }
