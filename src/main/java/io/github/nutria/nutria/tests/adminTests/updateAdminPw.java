4036d96967bd497d62c3488797a7430672d9e0af
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-10 20:05:25 -0300

    fix(updateAdminPw.java): refazer a lógica de atualização de senha do admin para fazer todas as senhas do banco ao mesmo tempo

--- src/main/java/io/github/nutria/nutria/tests/adminTests/updateAdminPw.java
+++ src/main/java/io/github/nutria/nutria/tests/adminTests/updateAdminPw.java
@@ -6,26 +6,35 @@ import io.github.nutria.nutria.util.PasswordHasher;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 
 public class updateAdminPw {
     public static void main(String[] args) {
         // Atualizar a senha do admin com id 4 (Enzo Mota) de "1234567810" para uma senha hasheada
         System.out.println("Testando atualização de senha do admin...");
 
-        LocalDate localDate = LocalDate.of(2010, 3, 12);
-        Admin admin = new Admin(4L, "Enzo D´Angio Mota", "enzo.mota@germinare.org.br", "1234567810", "11992866128", Date.valueOf(localDate), "Desenvolvedor", "");
         Admin admin1 = new Admin();
         AdminDAO dao = new AdminDAO();
 
+        List<Admin> admins = null;
 
-        System.out.println(dao.findAll(4));
+        for (int i = 0; i < dao.countAll(); i++) {
+            admins = dao.findAll((i < Math.ceil(dao.countAll()) / 4) ? (i + 1) : i);
+        }
 
-        System.out.println("Senha antes da atualização: " + admin.getSenha());
+        for (int i = 0; i < admins.size(); i++) {
+            admin1 = admins.get(i);
 
-        dao.update(admin);
+//            System.out.println("Senha antes da atualização: " + admin1.getSenha());
 
-        System.out.println("Senha após a atualização: " + dao.findAll(4).get(2).getSenha());
+            dao.update(admin1);
 
+//            System.out.println("Senha após a atualização: " + dao.findAll(4).get(2).getSenha());
+        }
+
+        System.out.println(dao.findAll(1));
+        System.out.println(dao.findAll(2));
+        System.out.println(dao.findAll(3));
         System.out.println(dao.findAll(4));
     }
 }
