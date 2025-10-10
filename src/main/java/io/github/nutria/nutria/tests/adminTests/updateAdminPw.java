20ad209a0566bdb6b934e2c73595d0579191d04a
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-10 19:37:48 -0300

    fix(updateAdminPw.java): atualizar informações do admin e ajustar ID na busca

--- src/main/java/io/github/nutria/nutria/tests/adminTests/updateAdminPw.java
+++ src/main/java/io/github/nutria/nutria/tests/adminTests/updateAdminPw.java
@@ -4,17 +4,21 @@ import io.github.nutria.nutria.dao.AdminDAO;
 import io.github.nutria.nutria.model.Admin;
 import io.github.nutria.nutria.util.PasswordHasher;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class updateAdminPw {
     public static void main(String[] args) {
         // Atualizar a senha do admin com id 4 (Enzo Mota) de "1234567810" para uma senha hasheada
         System.out.println("Testando atualização de senha do admin...");
 
-        Admin admin = new Admin(13L, "Mariana Marrão Ferreira Felis", "mariana.felis@germinare.org.br", "1234567810");
+        LocalDate localDate = LocalDate.of(2010, 3, 12);
+        Admin admin = new Admin(4L, "Enzo D´Angio Mota", "enzo.mota@germinare.org.br", "1234567810", "11992866128", Date.valueOf(localDate), "Desenvolvedor", "");
         Admin admin1 = new Admin();
         AdminDAO dao = new AdminDAO();
 
 
-        System.out.println(dao.findAll(3));
+        System.out.println(dao.findAll(4));
 
         System.out.println("Senha antes da atualização: " + admin.getSenha());
 
