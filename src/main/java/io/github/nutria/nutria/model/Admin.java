265083651d0dc234851343e5262c91ae8e475350
luismede <luishenrique23hh23@gmail.com>
2025-10-10 18:25:37 -0300

    feat(Admin): adicionar novos atributos e construtores para telefone, nascimento, cargo e foto

--- src/main/java/io/github/nutria/nutria/model/Admin.java
+++ src/main/java/io/github/nutria/nutria/model/Admin.java
@@ -1,21 +1,51 @@
 package io.github.nutria.nutria.model;
 
+import java.sql.Date;
+
 public class Admin {
     private Long id;
     private String nome;
     private String email;
     private String senha;
+    private String telefone;
+    private Date nascimento;
+    private String cargo;
+    private String foto;
 
     // Construtor sem argumentos
     public Admin() {
     }
 
     // Construtor com todos os argumentos
-    public Admin(Long id, String nome, String email, String senha) {
+    public Admin(Long id, String nome, String email, String senha, String telefone, Date nascimento, String cargo, String foto) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+        this.telefone = telefone;
+        this.nascimento = nascimento;
+        this.cargo = cargo;
+        this.foto = foto;
+    }
+
+
+    public Admin(String nome, String email, String senha, String telefone, Date nascimento, String cargo, String foto) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.nascimento = nascimento;
+        this.cargo = cargo;
+        this.foto = foto;
+    }
+
+    public Admin(String nome, String email, String senha, String telefone, Date nascimento, String cargo) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.nascimento = nascimento;
+        this.cargo = cargo;
     }
 
     public Admin(String nome, String email, String senha) {
@@ -55,6 +85,38 @@ public class Admin {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
     }
 
     // ToString
