42fe9e67a763fb80072af28a11c61328cf9d01b0
luismede <luishenrique23hh23@gmail.com>
2025-10-11 19:53:16 -0300

    style: renomear AdminstracaoServlet.java para HomeServlet.java e atualizar referências no servlet

--- src/main/java/io/github/nutria/nutria/servlet/HomeServlet.java
+++ src/main/java/io/github/nutria/nutria/servlet/HomeServlet.java
@@ -8,10 +8,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/administracao")
-public class AdminstracaoServlet extends HttpServlet {
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/administracao.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
     }
 }
