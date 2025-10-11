536417ffd1ac3a2ca0453ac6e4fbe3fe31df2938
luismede <luishenrique23hh23@gmail.com>
2025-10-11 12:32:11 -0300

    feat(admins): adicionar servlet para a página de administração

---
+++ src/main/java/io/github/nutria/nutria/servlet/AdminstracaoServlet.java
@@ -0,0 +1,17 @@
+package io.github.nutria.nutria.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/administracao")
+public class AdminstracaoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/administracao.jsp").forward(req, resp);
+    }
+}
