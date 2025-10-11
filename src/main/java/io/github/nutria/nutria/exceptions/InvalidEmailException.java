ce859454bb16f9654f37b50e588e732e2350c0e9
luismede <luishenrique23hh23@gmail.com>
2025-10-10 23:04:42 -0300

    fix(exception): atualizar mensagem de erro para formato de email inválido

--- src/main/java/io/github/nutria/nutria/exceptions/InvalidEmailException.java
+++ src/main/java/io/github/nutria/nutria/exceptions/InvalidEmailException.java
@@ -2,6 +2,6 @@ package io.github.nutria.nutria.exceptions;
 
 public class InvalidEmailException extends ValidationException {
     public InvalidEmailException(String email) {
-        super("email", String.format("Email inválido: %s", email));
+        super("email", String.format("Formato de mail inválido: %s", email));
     }
 }
