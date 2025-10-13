078c5a20241a0395266ddf9e1ffcf9a7a80f90a1
luismede <luishenrique23hh23@gmail.com>
2025-10-13 13:44:16 -0300

    feat(ReceitaSelectServlet): implementar servlet para listar receitas com tratamento de erros

---
+++ src/main/java/io/github/nutria/nutria/servlet/receitaServlet/ReceitaSelectServlet.java
@@ -0,0 +1,34 @@
+package io.github.nutria.nutria.servlet.receitaServlet;
+
+import io.github.nutria.nutria.dao.ReceitaDAO;
+import io.github.nutria.nutria.exceptions.DataAccessException;
+import io.github.nutria.nutria.model.Receita;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/receita/listar")
+public class ReceitaSelectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ReceitaDAO receitaDAO = new ReceitaDAO();
+
+        try {
+            int totalReceitas = receitaDAO.countAll();
+            List<Receita> receitasList = receitaDAO.findAll(1);
+
+            req.setAttribute("totalReceitas",totalReceitas);
+            req.setAttribute("receitasList", receitasList);
+
+            req.getRequestDispatcher("/WEB-INF/views/receita/receitas.jsp").forward(req, resp);
+
+        } catch (DataAccessException e) {
+            throw new DataAccessException("Erro ao acessar o banco de dados", e);
+        }
+    }
+}
