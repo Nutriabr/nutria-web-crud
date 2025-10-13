c344fc524f624ff3136a5580209d9d963e66a031
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-13 12:40:36 -0300

    feat(UsuarioUpdateServlet): implementar servlet para pré-preencher o formulário de edição do usuário

--- src/main/java/io/github/nutria/nutria/servlet/usuarioServlet/UsuarioUpdateServlet.java
+++ src/main/java/io/github/nutria/nutria/servlet/usuarioServlet/UsuarioUpdateServlet.java
@@ -1,5 +1,8 @@
 package io.github.nutria.nutria.servlet.usuarioServlet;
 
+import io.github.nutria.nutria.dao.UsuarioDAO;
+import io.github.nutria.nutria.exceptions.*;
+import io.github.nutria.nutria.model.Usuario;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,9 +11,27 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "UsuarioUpdateServlet", value = "/usuario/update")
+@WebServlet(name = "UsuarioUpdateServlet", value = "/usuario/atualizar")
 public class UsuarioUpdateServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String viewPath = "/WEB-INF/views/usuario/editar.jsp";
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        try {
+            Long id = Long.parseLong(req.getParameter("id"));
+
+            Usuario usuario = usuarioDAO.findById(id);
+            if (usuario != null) {
+                req.setAttribute("usuario", usuario);
+                req.getRequestDispatcher(viewPath).forward(req, resp);
+            } else {
+                resp.sendRedirect(req.getContextPath() + "/usuario/listar");
+            }
+        } catch (DataAccessException dae) {
+            throw new DataAccessException("Erro ao acessar o banco de dados", dae);
+        }
     }
 }
