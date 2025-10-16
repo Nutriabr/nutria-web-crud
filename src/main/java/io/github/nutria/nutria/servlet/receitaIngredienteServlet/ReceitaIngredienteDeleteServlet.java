6b3778ea98f041a143a323c3dcd50e4eae4f2fbc
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-16 09:06:38 -0300

    feat(ReceitaIngredienteDeleteServlet): trocar a rota do deletar

---
+++ src/main/java/io/github/nutria/nutria/servlet/receitaIngredienteServlet/ReceitaIngredienteDeleteServlet.java
@@ -0,0 +1,17 @@
+package io.github.nutria.nutria.servlet.receitaIngredienteServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/receitaIngrediente/deletar")
+public class ReceitaIngredienteDeleteServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
