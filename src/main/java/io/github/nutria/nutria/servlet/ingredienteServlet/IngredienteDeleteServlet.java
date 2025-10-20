f96da0ee9f6e9b484e4743871dae81fa50b55d89
marianamarrao <marimarrao0115@gmail.com>
2025-10-20 12:00:47 -0300

    feat(IngredienteDeleteServlet): adicionar servlet para deletar ingredientes

---
+++ src/main/java/io/github/nutria/nutria/servlet/ingredienteServlet/IngredienteDeleteServlet.java
@@ -0,0 +1,40 @@
+package io.github.nutria.nutria.servlet.ingredienteServlet;
+
+import io.github.nutria.nutria.dao.IngredienteDAO;
+import io.github.nutria.nutria.exceptions.DataAccessException;
+import io.github.nutria.nutria.exceptions.EntityNotFoundException;
+import io.github.nutria.nutria.exceptions.InvalidNumberException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/ingrediente/excluir")
+public class IngredienteDeleteServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idStr = req.getParameter("id");
+        Long id = Long.parseLong(idStr);
+
+        try {
+            IngredienteDAO ingredienteDAO = new IngredienteDAO();
+
+            ingredienteDAO.deleteById(id);
+
+            resp.sendRedirect(req.getContextPath() + "/ingrediente/listar");
+        }  catch (NumberFormatException e) {
+            req.setAttribute("errorMessage", "O ID informado é inválido.");
+            req.getRequestDispatcher("/WEB-INF/views/ingrediente/ingredientes.jsp").forward(req, resp);
+        } catch (InvalidNumberException | EntityNotFoundException e) {
+            req.setAttribute("errorMessage", e.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/ingrediente/ingredientes.jsp").forward(req, resp);
+        } catch (DataAccessException e) {
+            System.err.println("[ERRO INTERNO]: " + e);
+            req.setAttribute("errorMessage", "Erro ao acessar os dados. Tente novamente mais tarde.");
+            req.getRequestDispatcher("/WEB-INF/views/erro.jsp").forward(req, resp);
+        }
+
+    }
+}
