339dcbdacf0469cb7cfa21bde87071237c2ac1d8
marianamarrao <marimarrao0115@gmail.com>
2025-10-20 11:54:56 -0300

    feat(ProdutoDeleteServlet): (wip) adicionar servlet para deletar produtos

--- src/main/java/io/github/nutria/nutria/servlet/produtoServlet/ProdutoDeleteServlet.java
+++ src/main/java/io/github/nutria/nutria/servlet/produtoServlet/ProdutoDeleteServlet.java
@@ -1,6 +1,6 @@
-package io.github.nutria.nutria.servlet.receitaServlet;
+package io.github.nutria.nutria.servlet.produtoServlet;
 
-import io.github.nutria.nutria.dao.ReceitaDAO;
+import io.github.nutria.nutria.dao.ProdutoDAO;
 import io.github.nutria.nutria.exceptions.DataAccessException;
 import io.github.nutria.nutria.exceptions.EntityNotFoundException;
 import io.github.nutria.nutria.exceptions.InvalidNumberException;
@@ -11,27 +11,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.SQLException;
-
-@WebServlet("/receita/excluir")
-public class ReceitaDeleteServlet extends HttpServlet {
+@WebServlet("/produto/excluir")
+public class ProdutoDeleteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String idStr = req.getParameter("id");
         Long id = Long.parseLong(idStr);
 
         try {
-            ReceitaDAO receitaDAO = new ReceitaDAO();
+            ProdutoDAO produtoDAO = new ProdutoDAO();
 
-            receitaDAO.deleteById(id);
+            produtoDAO.deleteById(id);
 
-            resp.sendRedirect(req.getContextPath() + "/receita/listar");
+            resp.sendRedirect(req.getContextPath() + "/produto/listar");
         }  catch (NumberFormatException e) {
             req.setAttribute("errorMessage", "O ID informado é inválido.");
-            req.getRequestDispatcher("/WEB-INF/views/receita/receitas.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/views/produto/produtos.jsp").forward(req, resp);
         } catch (InvalidNumberException | EntityNotFoundException e) {
             req.setAttribute("errorMessage", e.getMessage());
-            req.getRequestDispatcher("/WEB-INF/views/receita/receitas.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/views/produto/produtos.jsp").forward(req, resp);
         } catch (DataAccessException e) {
             System.err.println("[ERRO INTERNO]: " + e);
             req.setAttribute("errorMessage", "Erro ao acessar os dados. Tente novamente mais tarde.");
