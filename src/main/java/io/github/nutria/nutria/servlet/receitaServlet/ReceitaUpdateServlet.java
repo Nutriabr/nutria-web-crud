f8f1313852a3f1d27af98e0cab5600995c35721c
marianamarrao <marimarrao0115@gmail.com>
2025-10-20 12:30:28 -0300

    feat(ReceitaUpdateServlet): adicionar servlet para atualizar receitas

---
+++ src/main/java/io/github/nutria/nutria/servlet/receitaServlet/ReceitaUpdateServlet.java
@@ -0,0 +1,78 @@
+package io.github.nutria.nutria.servlet.receitaServlet;
+
+import io.github.nutria.nutria.dao.ReceitaDAO;
+import io.github.nutria.nutria.exceptions.*;
+import io.github.nutria.nutria.model.Receita;
+import io.github.nutria.nutria.util.PasswordHasher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Date;
+
+@WebServlet("/receita/editar")
+public class ReceitaUpdateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ReceitaDAO receitaDAO = new ReceitaDAO();
+        String idStr = req.getParameter("id");
+
+        try {
+            if (idStr == null || idStr.isEmpty()) {
+                throw new InvalidNumberException("ID", idStr);
+            }
+            Long id = Long.parseLong(idStr);
+
+            Receita receita = receitaDAO.findById(id);
+
+            req.setAttribute("id", receita.getId());
+            req.setAttribute("porcao", receita.getPorcao());
+            req.setAttribute("id_produto", receita.getIdProduto());
+
+            req.getRequestDispatcher("/WEB-INF/views/receita/editar.jsp").forward(req, resp);
+        } catch (NumberFormatException e) {
+            req.setAttribute("errorMessage", "O ID informado é inválido.");
+            req.getRequestDispatcher("/WEB-INF/views/receita/editar.jsp").forward(req, resp);
+        } catch (InvalidNumberException | EntityNotFoundException e) {
+            req.setAttribute("errorMessage", e.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/receita/editar.jsp").forward(req, resp);
+        } catch (DataAccessException e) {
+            System.err.println("[ERRO INTERNO]: " + e);
+            req.setAttribute("errorMessage", "Erro ao acessar os dados. Tente novamente mais tarde.");
+            req.getRequestDispatcher("/WEB-INF/views/erro.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ReceitaDAO receitaDAO = new ReceitaDAO();
+        String viewPath = "/WEB-INF/views/receita/editar.jsp";
+
+        try {
+            Long id = Long.valueOf(req.getParameter("id"));
+            Receita receita = receitaDAO.findById(id);
+
+            receita.setPorcao(req.getParameter("porcao"));
+
+            receita.setIdProduto(Long.valueOf(req.getParameter("id_produto")));
+
+            receitaDAO.update(receita);
+
+            resp.sendRedirect(req.getContextPath() + "/receita/listar");
+
+        } catch (DuplicateEmailException | DuplicatePhoneException e) {
+            req.setAttribute("errorMessage", "Ops! " + e.getMessage());
+            req.getRequestDispatcher(viewPath).forward(req, resp);
+        } catch (EntityNotFoundException e) {
+            req.setAttribute("errorMessage", "Receita não encontrado para atualização.");
+            req.getRequestDispatcher(viewPath).forward(req, resp);
+        } catch (Exception e) {
+            req.setAttribute("errorMessage", "Ocorreu um erro inesperado: " + e.getMessage());
+            req.getRequestDispatcher(viewPath).forward(req, resp);
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/receita/editar.jsp
@@ -0,0 +1,61 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: luismedeiros-ieg
+  Date: 10/10/2025
+  Time: 18:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="utf-8" %>
+
+<%
+  Object id = request.getAttribute("id");
+  String porcao = (String) request.getAttribute("porcao");
+  Object id_produto = request.getAttribute("id_produto");
+  String contextPath = request.getContextPath();
+%>
+
+<html>
+<head>
+  <title>Editar Receita</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<%= contextPath %>/assets/css/forms.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+<%@include file="../components/messagemErro.jsp"%>
+<main>
+  <jsp:include page="/WEB-INF/views/components/sidebar.jsp">
+    <jsp:param name="activePage" value="receita"/>
+  </jsp:include>
+  <div class="container">
+    <div class="main-content">
+      <h1>Editar Receita</h1>
+      <p>Preencha as novas informações da receita.</p>
+    </div>
+
+
+    <form class="form-content" name="forms-add-admin" action="<%= contextPath %>/receita/editar" method="post">
+      <h2>Informações da Receita</h2>
+
+      <input type="hidden" name="id" value="<%= id %>">
+
+      <label for="porcao-input">Porção</label>
+      <input class="parameter-input" type="text" id="porcao-input" name="porcao" value="<%= porcao %>">
+
+      <label for="id-produto-input">ID-Produto</label>
+      <input class="parameter-input" type="text" id="id-produto-input" name="id-produto" value="<%= id_produto %>">
+
+      <div class="submit-content">
+        <a href="<%= contextPath %>/produto/listar" id="btn-cancel">Cancelar</a>
+        <input id="submit-btn" type="submit" value="Salvar Alterações">
+      </div>
+    </form>
+
+    <%--        <% } %>--%>
+  </div>
+</main>
+<script src="${pageContext.request.contextPath}/assets/js/validator.js"></script>
+
+</body>
+</html>
