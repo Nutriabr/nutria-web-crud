809e38c72390cd460f06fa039b68c12f38215518
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-25 20:32:51 -0300

    feat(IAdminDAO): adicionar documentação e implementar os métodos buscar por email, buscar por buscar por dominio email, buscar por telefone e buscar por nome

--- src/main/java/io/github/nutria/nutria/dao/interfaces/IAdminDAO.java
+++ src/main/java/io/github/nutria/nutria/dao/interfaces/IAdminDAO.java
@@ -3,6 +3,7 @@ package io.github.nutria.nutria.dao.interfaces;
 import io.github.nutria.nutria.exceptions.DataAccessException;
 import io.github.nutria.nutria.model.Admin;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -21,5 +22,32 @@ public interface IAdminDAO {
      * @return um {@link Optional} contendo o {@link Admin} correspondente ao email informado, ou vazio se não encontrado.
      * @throws DataAccessException se ocorrer algum erro ao acessar o banco de dados.
      */
-    Optional<Admin> findByEmail(String email);
+    Optional<Admin> buscarPorEmail(String email);
+
+    /**
+     * Lista todos os registros de {@link Admin} com o domínio de email informado.
+     *
+     * @param dominio o domínio do endereço de email que será utilizado na busca.
+     * @return um {@link Optional} contendo uma lista de objetos {@link Admin} correspondentes ao dominio informado, ou vazio se não encontrado.
+     * @throws DataAccessException se ocorrer algum erro ao acessar o banco de dados.
+     */
+    Optional<List<Admin>> buscarPorDominioEmail(String dominio);
+
+    /**
+     * Lista todos os registros de {@link Admin} com o nome informado.
+     *
+     * @param nome o nome que será utilizado na busca.
+     * @return um {@link Optional} contendo uma lista de objetos {@link Admin} correspondentes ao nome informado, ou vazio se não encontrado.
+     * @throws DataAccessException se ocorrer algum erro ao acessar o banco de dados.
+     */
+    Optional<List<Admin>> buscarPorNome(String nome);
+
+    /**
+     * Busca um registro de {@link Admin} pelo telefone informado.
+     *
+     * @param fone o telefone que será utilizado na busca.
+     * @return um {@link Optional} contendo o {@link Admin} correspondente ao telefone informado, ou vazio se não encontrado.
+     * @throws DataAccessException se ocorrer algum erro ao acessar o banco de dados.
+     */
+    Optional<Admin> buscarPorTelefone(String fone);
 }
