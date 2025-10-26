8e198eb9fcfef3196677477b01c583ea7b879b03
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-25 22:04:18 -0300

    feat(PasswordHasher): criar a classe com método estático

--- src/main/java/io/github/nutria/nutria/util/PasswordHasher.java
+++ src/main/java/io/github/nutria/nutria/util/PasswordHasher.java
@@ -8,9 +8,18 @@ public class PasswordHasher {
 
     /** Método estático responsável por gerar o hash da semanha, usando um workload 12
      * @return String - Retorna a senha com o hash armazenado
-     * @param plaintextPassword - Recebe a senha da requisição que será feita o hash
+     * @param senhaOriginal - Recebe a senha da requisição que será feita o hash
      * */
-    public static String hashPassword(String plaintextPassword) {
-        return BCrypt.hashpw(plaintextPassword, BCrypt.gensalt(workload));
+    public static String hashSenha(String senhaOriginal) {
+        return BCrypt.hashpw(senhaOriginal, BCrypt.gensalt(workload));
+    }
+
+    /** Método estático responsável por verificar se a senha original condiz com o hash correto.
+     * @return boolean - Retorna true caso a senha seja igual o hash armazenado
+     * @param senhaRecebida - Recebe a senha da requisição que será feita o hash
+     * @param senhaArmezanada - Recebe o hash armazenado
+     * */
+    public static boolean verificarSenha(String senhaRecebida, String senhaArmezanada) {
+        return BCrypt.checkpw(senhaRecebida, senhaArmezanada);
     }
 }
