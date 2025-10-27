0b9a14f7e26156ff13cc468c6db962f94d4d080b
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-27 12:25:17 -0300

    refactor(receitaIngrediente/editar.jsp): traduzir o nome do atributo para português e adicionar a mensagem de sucesso

--- src/main/java/io/github/nutria/nutria/servlet/receitaIngredienteServlet/ReceitaIngredienteInsertServlet.java
+++ src/main/java/io/github/nutria/nutria/servlet/receitaIngredienteServlet/ReceitaIngredienteInsertServlet.java
@@ -17,8 +17,7 @@ public class ReceitaIngredienteInsertServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String currentPage = req.getParameter("page");
-        final String viewPath = "/WEB-INF/views/receitaIngrediente/adicionar.jsp";
-        req.getRequestDispatcher(viewPath).forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/receitaIngrediente/adicionar.jsp").forward(req, resp);
     }
 
     @Override
@@ -29,14 +28,14 @@ public class ReceitaIngredienteInsertServlet extends HttpServlet {
         int lastPage = (int) Math.ceil((double) receitaIngredienteDAO.contarTodos() / 4);
 
         try {
-            receitaIngrediente.setIdReceita(Long.parseLong(req.getParameter("id")));
             receitaIngrediente.setIdReceita(Long.parseLong(req.getParameter("idReceita")));
             receitaIngrediente.setIdIngrediente(Long.parseLong(req.getParameter("idIngrediente")));
-            receitaIngrediente.setQuantidade(Integer.parseInt(req.getParameter("quantity")));
+            receitaIngrediente.setQuantidade(Integer.parseInt(req.getParameter("quantidade")));
 
             receitaIngredienteDAO.inserir(receitaIngrediente);
 
-            resp.sendRedirect(req.getContextPath() + "/receitaIngrediente/listar?page=" + (lastPage + 1));
+            req.getSession().setAttribute("successMessage", "Receita Ingrediente inserida com sucesso!");
+            resp.sendRedirect(req.getContextPath() + "/receitasIngredientes/listar?page=" + (lastPage + 1));
         } catch (RequiredFieldException rfe) {
             System.err.println("[ERRO DE CAMPO OBRIGATÓRIO]: " + rfe);
             req.setAttribute("quantidade", req.getParameter("quantity"));
