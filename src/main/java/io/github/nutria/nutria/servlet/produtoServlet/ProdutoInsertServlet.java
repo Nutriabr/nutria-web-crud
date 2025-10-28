552439ef72532d3f945582cfda68b87d1688e130
marianamarrao <marimarrao0115@gmail.com>
2025-10-28 18:56:12 -0300

    fix(ProdutoInsertServlet): tratar exceção de quando a entidade não existir com o email colocado

--- src/main/java/io/github/nutria/nutria/servlet/produtoServlet/ProdutoInsertServlet.java
+++ src/main/java/io/github/nutria/nutria/servlet/produtoServlet/ProdutoInsertServlet.java
@@ -3,6 +3,7 @@ package io.github.nutria.nutria.servlet.produtoServlet;
 import io.github.nutria.nutria.dao.ProdutoDAO;
 import io.github.nutria.nutria.dao.UsuarioDAO;
 import io.github.nutria.nutria.exceptions.DataAccessException;
+import io.github.nutria.nutria.exceptions.EntityNotFoundException;
 import io.github.nutria.nutria.exceptions.RequiredFieldException;
 import io.github.nutria.nutria.exceptions.ValidationException;
 import io.github.nutria.nutria.model.Produto;
@@ -29,23 +30,27 @@ public class ProdutoInsertServlet extends HttpServlet {
         String emailUsuario = req.getParameter("emailUsuario");
         ProdutoDAO produtoDAO = new ProdutoDAO();
         UsuarioDAO usuarioDAO = new UsuarioDAO();
-        Optional<Usuario> optionalUsuario = usuarioDAO.buscarPorEmail(emailUsuario);
-        Usuario usuario = new Usuario();
-        Long idUsuario;
-        if (optionalUsuario.isPresent()) {
-             usuario = optionalUsuario.get();
-        }
-        idUsuario = usuario.getId();
-        boolean success;
-        Produto produto = new Produto(nome,idUsuario);
 
         try {
+            Optional<Usuario> optionalUsuario = usuarioDAO.buscarPorEmail(emailUsuario);
+            Usuario usuario = new Usuario();
+            Long idUsuario;
+            if (optionalUsuario.isPresent()) {
+                usuario = optionalUsuario.get();
+            }
+            idUsuario = usuario.getId();
+            boolean success;
+            Produto produto = new Produto(nome,idUsuario);
             success = produtoDAO.inserir(produto);
             if (!success) throw new DataAccessException("Erro ao inserir produto no banco de dados.");
 
-            req.getSession().setAttribute("message", "Produto inserido com sucesso!");
+            req.getSession().setAttribute("successMessage", "Produto inserido com sucesso!");
             resp.sendRedirect(req.getContextPath() + "/produto/listar");
 
+        } catch (EntityNotFoundException enfe){
+            System.err.println("[ERRO DE ENTIDADE NÃO ACHADA]");
+            req.setAttribute("errorMessage","Ops! " + enfe.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/produto/adicionar.jsp").forward(req,resp);
         } catch (RequiredFieldException rfe){
             System.err.println("[ERRO DE CAMPOS OBRIGATÓRIOS AUSENTES]");
             req.setAttribute("errorMessage", "Ops! " + rfe.getMessage());
