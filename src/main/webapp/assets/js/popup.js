d7f40435a82085cbf6c047e2ffba71d9f3be6559
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-30 17:55:29 -0300

    feat(popup.js): adicionar funcionamento do popup personalizado de deletar todos os usuários pelo nome da empresa

--- src/main/webapp/assets/js/popup.js
+++ src/main/webapp/assets/js/popup.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+    deletarPorId();
+    deletarPorEmpresa();
+});
+
+function deletarPorId() {
     const deletePopupOverlay = document.getElementById("delete-popup-overlay");
     const cancelBtn = document.getElementById("cancel-delete-btn");
     const inputId = document.getElementById("input-id")
@@ -29,4 +34,37 @@ document.addEventListener("DOMContentLoaded", function () {
             deleteForm.reset();
         }
     });
-});
+}
+
+function deletarPorEmpresa() {
+    const deletePopupOverlay = document.getElementById("delete-all-popup-overlay");
+    const deleteAllform = document.getElementById("delete-all-form");
+    const empresaNome = document.getElementById("empresa-nome");
+    const inputEmpresa = document.getElementById("input-empresa");
+    const cancelBtn = document.getElementById("cancel-delete-all-btn");
+    const deleteButton = document.querySelectorAll(".deletar-por-empresa");
+
+    deleteButton.forEach(button => {
+        button.addEventListener("click", function () {
+            const opcao = document.getElementById("selectEmpresa").value;
+
+            empresaNome.value = opcao;
+
+            inputEmpresa.value = opcao;
+
+            deletePopupOverlay.style.display = "flex";
+        });
+    });
+
+    cancelBtn.addEventListener("click", function () {
+        deletePopupOverlay.style.display = "none";
+        deleteForm.reset();
+    });
+
+    deletePopupOverlay.addEventListener("click", function (event) {
+        if (event.target === deletePopupOverlay) {
+            deletePopupOverlay.style.display = "none";
+            deleteAllform.reset();
+        }
+    });
+}
