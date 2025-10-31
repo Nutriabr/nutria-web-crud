c8f696a7a6291a433614efaf17680acdd137bef7
EnzoDangioMota <enzo.mota@germinare.org.br>
2025-10-31 10:18:01 -0300

    refactor(formAutoSubmit.js): deletar .js que envia o formulário automaticamente quando ele estiver vazio para facilitar a listagem padrão, sem buscas

--- src/main/webapp/assets/js/formAutoSubmit.js
+++
@@ -1,8 +0,0 @@
-const formBusca = document.getElementById('form-busca');
-const busca = document.getElementById("input-busca");
-
-busca.addEventListener("input", () => {
-    if (busca.value.trim() === "") {
-        formBusca.submit();
-    }
-});
